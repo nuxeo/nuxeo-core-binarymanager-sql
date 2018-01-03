a719d073ba3e369292e1ba1e14a8ae75a426fa90
Kevin Leturc <kleturc@nuxeo.com>
2018-01-03 15:02:53 +0100

    NXP-24090: Replace commons-lang by commons-lang3

--- src/main/java/org/nuxeo/ecm/core/storage/sql/SQLBinaryManager.java
+++ src/main/java/org/nuxeo/ecm/core/storage/sql/SQLBinaryManager.java
@@ -32,7 +32,7 @@ import java.sql.SQLException;
 import java.util.Map;
 
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.nuxeo.ecm.core.blob.binary.Binary;
